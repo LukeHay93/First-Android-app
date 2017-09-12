629e46371b76c3579c6e94b8228e7f41c76b774f
LukeHay93 <luke_15hayward@hotmail.com>
2017-09-12 09:39:06 +0100

    t# t quit git push

---
+++ app/src/main/java/com/example/luke/messingaround/Torch.java
@@ -0,0 +1,13 @@
+package com.example.luke.messingaround;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Torch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_torch);
+    }
+}
